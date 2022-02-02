4af9534e6767faca2e1995eb6c4155b5d697f514
Josua Müller <mueller.j.at@gmail.com>
2022-02-02 05:36:16 +0100

    feat: implement fires through random points

--- main.lua
+++ main.lua
@@ -1,8 +1,4 @@
-strength = 1 -- Debris Blow away Power
-maxMass = 100 -- Debris Weight Affected by Blast
-maxDist = 20 -- Debris Distance Affected by Blast
-power = .1
-cooldownTimer = 0
+nozzleOffsetTransform = Transform(Vec(0.3, -0.3, -1.2))
 
 function init()
     RegisterTool("flamethrower", "Flamethrower", "MOD/vox/Flamethrower.vox")
@@ -14,97 +10,78 @@ function init()
 end
 
 function tick(dt)
-	if GetString("game.player.tool") == "flamethrower" and GetBool("game.player.canusetool") then
-		SetBool("hud.aimdot", false)
-	
-		-- Ignition and Extinguish sound effects for the Flamethrower
-		if GetString("game.player.tool") == "flamethrower" and InputPressed("lmb") then
-			PlaySound(soundIgnition, GetPlayerTransform().pos, (math.random(50, 60) * 0.01))
-		end
-		if GetString("game.player.tool") == "flamethrower" and InputReleased("lmb") then
-			PlaySound(soundExtinguish, GetPlayerTransform().pos, (math.random(10, 20) * 0.01))
-		end
-	
-		-- Resting Tool Position
-		local offset = Transform(Vec(0.3, -0.3, -.74))
-		SetToolTransform(offset, 0.6)
-	
-		-- Minimum Tool Recoil
-		if InputDown("lmb") then
-			local offset = Transform(Vec(0.3, -0.3, -.71))
-			SetToolTransform(offset, 0.3)
-		end
-	
-		-- Check if Left Mouse Button is Pressed Down activating Flamethrower Flames
-		if InputDown("lmb") then
-			-- Get fire spawn locations in front of Player
-			local t = GetCameraTransform()
-			local points = {
-				TransformToParentPoint(t, Vec(0.3, -0.3, -1)),
-				TransformToParentPoint(t, Vec(0.3, -0.3, -2.5)),
-				TransformToParentPoint(t, Vec(0.7, -0.7, -2.8)),
-				TransformToParentPoint(t, Vec(0.7, .1, -3.1)),
-				TransformToParentPoint(t, Vec(-0.1, -0.7, -3.4)),
-				TransformToParentPoint(t, Vec(-0.1, .1, -3.7)),
-				TransformToParentPoint(t, Vec(0.2, -0.2, -4)),
-				TransformToParentPoint(t, Vec(0.7, -0.7, -4.3)),
-				TransformToParentPoint(t, Vec(0.7, 0.3, -4.6)),
-				TransformToParentPoint(t, Vec(-0.3, -0.7, -4.9)),
-				TransformToParentPoint(t, Vec(-0.3, 0.3, -5.2)),
-				TransformToParentPoint(t, Vec(0.2, -0.2, -5.5)),
-				TransformToParentPoint(t, Vec(0.8, -0.8, -5.8)),
-				TransformToParentPoint(t, Vec(0.8, 0.4, -6.1)),
-				TransformToParentPoint(t, Vec(-0.4, -0.8, -6.4)),
-				TransformToParentPoint(t, Vec(-0.4, 0.4, -6.7)),
-				TransformToParentPoint(t, Vec(0.1, -0.1, -7)),
-				TransformToParentPoint(t, Vec(0.8, -0.8, -7.3)),
-				TransformToParentPoint(t, Vec(0.8, 0.6, -7.6)),
-				TransformToParentPoint(t, Vec(-0.6, -0.8, -7.9)),
-				TransformToParentPoint(t, Vec(-0.6, 0.6, -8.2)),
-				TransformToParentPoint(t, Vec(0.1, -0.1, -8.5)),
-				TransformToParentPoint(t, Vec(0.9, -0.9, -8.8)),
-				TransformToParentPoint(t, Vec(0.9, 0.7, -9.1)),
-				TransformToParentPoint(t, Vec(-0.7, -0.9, -9.4)),
-				TransformToParentPoint(t, Vec(-0.7, 0.7, -9.7)),
-				TransformToParentPoint(t, Vec(0.1, -0.1, -10)),
-				TransformToParentPoint(t, Vec(0.7, -0.7, -10.5)),
-				TransformToParentPoint(t, Vec(0.7, 0.5, -11)),
-				TransformToParentPoint(t, Vec(-0.5, -0.7, -11.5)),
-				TransformToParentPoint(t, Vec(-0.5, 0.5, -12)),
-				TransformToParentPoint(t, Vec(0, 0, -12.5)),
-				TransformToParentPoint(t, Vec(0.4, -0.4, -13)),
-				TransformToParentPoint(t, Vec(0.4, 0.4, -13.5)),
-				TransformToParentPoint(t, Vec(-0.4, -0.4, -14)),
-				TransformToParentPoint(t, Vec(-0.4, 0.4, -14.5)),
-				TransformToParentPoint(t, Vec(0, 0, -15)),
-				TransformToParentPoint(t, Vec(0.3, -0.3, -15.5)),
-				TransformToParentPoint(t, Vec(-0.3, 0.3, -16)),
-			}
-	
-			-- make tons of fire if something combustible is there
-			for _, p in ipairs(points) do
-				SpawnFire(p)
-			end
-		end
-	end
+    if GetString("game.player.tool") == "flamethrower" and GetBool("game.player.canusetool") then
+        SetBool("hud.aimdot", false)
+
+        -- Ignition and Extinguish sound effects for the Flamethrower
+        if GetString("game.player.tool") == "flamethrower" and InputPressed("lmb") then
+            PlaySound(soundIgnition, GetPlayerTransform().pos, (math.random(50, 60) * 0.01))
+        end
+        if GetString("game.player.tool") == "flamethrower" and InputReleased("lmb") then
+            PlaySound(soundExtinguish, GetPlayerTransform().pos, (math.random(10, 20) * 0.01))
+        end
+
+        if InputDown("lmb") then
+            -- Minimum Tool Recoil
+            local offset = Transform(Vec(0.3, -0.3, -.71))
+            SetToolTransform(offset, 0.3)
+
+            -- Get fire spawn locations in front of Player
+			local camera = GetCameraTransform()
+			local nozzle = TransformToParentTransform(camera, nozzleOffsetTransform)
+			local fwd = TransformToParentVec(nozzle, Vec(0, 0, -1))
+			local maxDist = 10;
+			local hit, dist, normal, shape = QueryRaycast(nozzle.pos, fwd, maxDist)
+
+            if hit then
+				local hitPoint = Transform(VecAdd(nozzle.pos, VecScale(fwd, dist)), camera.rot)
+				DebugLine(nozzle.pos, hitPoint.pos)
+
+				for i = 1, 100, 1 do
+					local point = randomPoint(hitPoint.pos, 1)
+					SpawnFire(point)
+                    DebugCross(point)
+				end
+            end
+        else
+            -- Resting Tool Position
+            local offset = Transform(Vec(0.3, -0.3, -.74))
+            SetToolTransform(offset, 0.6)
+        end
+    end
+end
+
+function randomPoint(offsetFrom, radius)
+	local radius = radius * 100
+	local offsetLength = math.random(-radius, radius) / 100
+	local offsetRotation = QuatEuler(
+		math.random(0, 360),
+		math.random(0, 360),
+		math.random(0, 360)
+	)
+
+	return VecAdd(
+		offsetFrom,
+		QuatRotateVec(
+			offsetRotation,
+			Vec(0, 0, offsetLength)
+		)
+	)
 end
 
 function update(dt)
-    if cooldownTimer > 0 then
-        cooldownTimer = cooldownTimer - dt
-    end
-
     if GetString("game.player.tool") == "flamethrower" and GetBool("game.player.canusetool") then
         -- Compute hit points and front direction of Player Weapon in world space
-        local t = GetCameraTransform()
-        local pp = TransformToParentPoint(t, Vec(.45, -.5, -1.65))
-        local d = TransformToParentVec(t, Vec(-0.07, .06, -1))
+        local camera = GetCameraTransform()
+		local nozzle = TransformToParentTransform(camera, nozzleOffsetTransform)
+		DebugCross(nozzle.pos, 0, 255, 0)
+        local d = TransformToParentVec(camera, Vec(-0.07, .06, -1))
 
         if InputDown("lmb") then -- Flamethrower Flame Effects
-            local v = GetPlayerVelocity()
-            v = VecAdd(v, VecScale(d, -.1))
+            local playerVelocity = GetPlayerVelocity()
+            playerVelocity = VecAdd(playerVelocity, VecScale(d, -.1))
 
-            local pvel = VecScale(v, .7)
+            local pvel = VecScale(playerVelocity, .7)
             ParticleReset()
             ParticleColor(1, math.random(40, 50) * .01, 0, 1, math.random(20, 40) * .01, 0)
             ParticleEmissive(1, 0)
@@ -114,7 +91,8 @@ function update(dt)
             ParticleAlpha(1, 0)
             ParticleGravity(0, 20)
             ParticleDrag(0, .3)
-            SpawnParticle(pp, VecAdd(pvel, VecScale(d, 30)), .7) -- medium orange to red
+            SpawnParticle(nozzle.pos, VecAdd(pvel, VecScale(d, 30)), .7) -- medium orange to red
+
             ParticleColor(1, math.random(75, 85) * .01, .1, 1, math.random(55, 65) * .01, .4)
             ParticleEmissive(3, 0)
             ParticleCollide(.01)
@@ -123,7 +101,8 @@ function update(dt)
             ParticleAlpha(1, 0)
             ParticleGravity(0, 20)
             ParticleDrag(0, .3)
-            SpawnParticle(pp, VecAdd(pvel, VecScale(d, 30)), .7) -- smaller yellow to orange
+            SpawnParticle(nozzle.pos, VecAdd(pvel, VecScale(d, 30)), .7) -- smaller yellow to orange
+
             ParticleColor(1, 1, .8, .8, .8, math.random(50, 60) * .01)
             ParticleEmissive(5, 0)
             ParticleCollide(0.01)
@@ -132,7 +111,8 @@ function update(dt)
             ParticleAlpha(.5, 0, "easeout")
             ParticleGravity(0, 20)
             ParticleDrag(0, .3)
-            SpawnParticle(pp, VecAdd(pvel, VecScale(d, 30)), .7) -- smallest white to white/yellow
+            SpawnParticle(nozzle.pos, VecAdd(pvel, VecScale(d, 30)), .7) -- smallest white to white/yellow
+
             ParticleRadius(.1, 5)
             ParticleColor(0, 0, 0, .1, .1, .1)
             ParticleTile(14)
@@ -140,10 +120,10 @@ function update(dt)
             ParticleAlpha(.6, 0)
             ParticleGravity(0, 20)
             ParticleDrag(0, .3)
-            SpawnParticle(pp, VecAdd(pvel, VecScale(d, 30)), 1.8) -- bigger black
+            SpawnParticle(nozzle.pos, VecAdd(pvel, VecScale(d, 30)), 1.8) -- bigger black
+
             -- Play Flamethrower sound
             PlayLoop(soundIncinerate, GetPlayerTransform().pos, (math.random(5, 7) * 0.1))
         end
     end
 end
-
