3c44d8db0dd140012c3d30bafe19f007fcd656cf
Josua Müller <mueller.j.at@gmail.com>
2022-02-03 20:18:35 +0100

    wip: separate knob on model, add knob class for future features

---
+++ scripts/knob.lua
@@ -0,0 +1,24 @@
+Knob = {
+    nozzleDegree = 30,
+    nozzleDegreeMin = 10,
+    nozzleDegreeMax = 90,
+}
+
+function Knob:update()
+    tool = GetToolBody()
+
+    shapes = GetBodyShapes(tool)
+    knobShape = shapes[2]
+    -- DrawShapeOutline(knobShape, 255, 255, 0, 1)
+
+    if InputPressed("lmb") then
+        SetShapeEmissiveScale(knobShape, 100)
+    end
+
+    knobBody = GetShapeBody(knobShape)
+    DrawBodyOutline(knobBody,255,0,0,0.8)
+
+    if InputDown('q') then
+        
+    end
+end
