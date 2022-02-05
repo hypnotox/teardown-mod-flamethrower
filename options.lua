bdf7387d6145b0d1c5e6ac55dfc507a361433243
Josua Müller <hypnotox@pm.me>
2022-02-05 07:13:16 +0100

    feat: implement basic options panel

---
+++ options.lua
@@ -0,0 +1,173 @@
+Options = {
+    valueToSet = nil,
+    inputBuffer = nil,
+}
+
+function init()
+    local isInitialized = GetBool('savegame.mod.general.is_initialized')
+
+    if not isInitialized or true then
+        SetBool('savegame.mod.features.fire_limit.enabled', true)
+        SetInt('savegame.mod.features.fire_limit.value', 1000000)
+        SetString('savegame.mod.features.nozzle.keybinds.decrease', 'leftarrow')
+        SetString('savegame.mod.features.nozzle.keybinds.increase', 'rightarrow')
+
+        SetBool('savegame.mod.general.is_initialized', true)
+    end
+end
+
+function draw()
+    UiTranslate(UiCenter(), 250)
+    UiAlign('center middle')
+    UiFont('regular.ttf', 24)
+
+    --Title
+    UiPush()
+    UiFont('bold.ttf', 48)
+    UiText('Flamethrower mod options')
+    UiPop()
+
+    -- Subtitle nozzle
+    Options:subtitle('Nozzle adjustment')
+
+    UiTranslate(0, 100)
+    Options:keybind('Decrease nozzle velocity', 'savegame.mod.features.nozzle.keybinds.decrease')
+    Options:keybind('Increase nozzle velocity', 'savegame.mod.features.nozzle.keybinds.increase')
+
+    -- Subtitle fire limit
+    Options:subtitle('Fire limit')
+    Options:toggle('Enable fire limit', 'savegame.mod.features.fire_limit.enabled')
+
+    UiTranslate(0, 100)
+    if UiTextButton('Close', 200, 40) then
+        Menu()
+    end
+end
+
+function Options:title(title)
+    UiPush()
+    UiFont('bold.ttf', 48)
+    UiText(title)
+    UiPop()
+    UiTranslate(0, 100)
+end
+
+function Options:subtitle(title)
+    UiPush()
+    UiFont('bold.ttf', 36)
+    UiText(title)
+    UiPop()
+    UiTranslate(0, 100)
+end
+
+function Options:keybind(title, key)
+    UiPush()
+    UiFont('regular.ttf', 26)
+
+    UiPush()
+    UiTranslate(-200, 0)
+    UiText(title)
+    UiPop()
+
+    UiPush()
+    UiTranslate(200, 0)
+    local keybind = GetString(key)
+
+    if keybind and keybind ~= nil then
+        UiPush()
+        UiColor(0.5, 1, 0.5, 0.2)
+        UiImageBox('ui/common/box-solid-6.png', 200, 40, 6, 6)
+        UiPop()
+    end
+
+    if Options.keybindToSet == key then
+        UiButtonImageBox('ui/common/box-outline-6.png', 6, 6)
+    end
+
+    if UiTextButton(keybind or 'not bound', 200, 40) then
+        Options.keybindToSet = key
+    end
+    UiPop()
+
+    UiPop()
+    UiTranslate(0, 100)
+
+    local lastPressedKey = InputLastPressedKey()
+    if Options.keybindToSet ~= nil and lastPressedKey ~= '' then
+        SetString(Options.keybindToSet, lastPressedKey)
+        Options.keybindToSet = nil
+    end
+end
+
+function Options:toggle(title, key)
+    UiPush()
+
+    UiPush()
+    UiTranslate(-200, 0)
+    UiText(title)
+    UiPop()
+
+    UiPush()
+    UiTranslate(200, 0)
+    UiButtonImageBox('ui/common/box-outline-6.png', 6, 6)
+    local label = 'Off'
+
+    if GetBool(key) then
+        UiPush()
+        UiColor(0.5, 1, 0.5, 0.2)
+        UiImageBox('ui/common/box-solid-6.png', 80, 40, 6, 6)
+        UiPop()
+        label = 'On'
+    end
+
+    if UiTextButton(label, 80, 40) then
+        SetBool(key, not GetBool(key))
+    end
+    UiPop()
+
+    UiPop()
+    UiTranslate(0, 100)
+end
+
+function Options:input(title, key)
+    UiPush()
+    UiFont('regular.ttf', 26)
+
+    UiPush()
+    UiTranslate(-200, 0)
+    UiText(title)
+    UiPop()
+
+    UiPush()
+    UiTranslate(200, 0)
+    local keybind = GetString(key)
+
+    if keybind and keybind ~= nil then
+        UiPush()
+        UiColor(0.5, 1, 0.5, 0.2)
+        UiImageBox('ui/common/box-solid-6.png', 200, 40, 6, 6)
+        UiPop()
+    end
+
+    if Options.valueToSet == key then
+        UiButtonImageBox('ui/common/box-outline-6.png', 6, 6)
+    end
+
+    if UiTextButton(GetInt(key), 200, 40) then
+        Options.valueToSet = key
+    end
+    UiPop()
+
+    UiPop()
+    UiTranslate(0, 100)
+
+    local lastPressedKey = InputLastPressedKey()
+    if Options.valueToSet ~= nil and lastPressedKey ~= '' then
+        if lastPressedKey == 'enter' then
+            Options.valueToSet = nil
+            SetInt(Options.valueToSet, Options.inputBuffer)
+        else
+            Options.inputBuffer = Options.inputBuffer .. lastPressedKey
+        end
+    end
+end
--- scripts/knob.lua
+++ scripts/knob.lua
@@ -3,7 +3,13 @@ Knob = {
     flameVelocityMin = 5,
     flameVelocityMax = 40,
     changePerSecond = 10,
+    keybinds = {},
 }
+
+function Knob:init()
+    self.keybinds.decrease = GetString('savegame.mod.features.nozzle.keybinds.decrease')
+    self.keybinds.increase = GetString('savegame.mod.features.nozzle.keybinds.increase')
+end
 
 function Knob:tick()
     Debug:shapeOutline(knobShape)
@@ -12,14 +18,14 @@ function Knob:tick()
         SetShapeEmissiveScale(knobShape, 100)
     end
 
-    if InputDown('leftarrow') and not InputDown('rightarrow') and self.flameVelocity >= self.flameVelocityMin then
+    if InputDown(self.keybinds.decrease) and not InputDown(self.keybinds.increase) and self.flameVelocity >= self.flameVelocityMin then
         local change = self.changePerSecond * GetTimeStep()
 
         self.flameVelocity = self.flameVelocity - change
         self:rotateKnob(-change)
     end
 
-    if InputDown('rightarrow') and not InputDown('leftarrow') and self.flameVelocity <= self.flameVelocityMax then
+    if InputDown(self.keybinds.increase) and not InputDown(self.keybinds.decrease) and self.flameVelocity <= self.flameVelocityMax then
         local change = self.changePerSecond * GetTimeStep()
 
         self.flameVelocity = self.flameVelocity + change
