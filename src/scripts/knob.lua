991557cc054e326ca34156f99bbdb51cc9824a51
Josua Müller <hypnotox@pm.me>
2022-02-07 00:23:10 +0100

    feat: rotate knob around center axis

--- src/scripts/knob.lua
+++ src/scripts/knob.lua
@@ -25,27 +25,28 @@ function Knob:tick()
         local change = self.changePerSecond * GetTimeStep()
 
         self.flameVelocity = self.flameVelocity - change
-        self:rotateKnob(-change)
+        self:rotateKnob(change)
     end
 
     if InputDown(self.keybinds.increase) and not InputDown(self.keybinds.decrease) and self.flameVelocity <= self.flameVelocityMax then
         local change = self.changePerSecond * GetTimeStep()
 
         self.flameVelocity = self.flameVelocity + change
-        self:rotateKnob(change)
+        self:rotateKnob(-change)
     end
 end
 
-function Knob:rotateKnob(degree)
-    local knobShape = self:getShape()
-    local knobTransform = GetShapeLocalTransform(knobShape)
+function Knob:rotateKnob(angle)
+    local shape = self:getShape()
+    local axisTransform = Transform(Vec(Engine.voxelSize * 0.5, Engine.voxelSize * 6.5, 0))
+    local shapeTransform = TransformToLocalTransform(axisTransform, GetShapeLocalTransform(shape))
+
+    axisTransform.rot = QuatEuler(0, 0, angle)
+    shapeTransform = TransformToParentTransform(axisTransform, shapeTransform)
 
     SetShapeLocalTransform(
-        knobShape,
-        Transform(
-            knobTransform.pos,
-            QuatRotateQuat(knobTransform.rot, QuatEuler(0, degree, 0))
-        )
+        shape,
+        shapeTransform
     )
 end
 
