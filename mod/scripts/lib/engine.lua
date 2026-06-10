8586d7654bf36f5ac55d813ca48373ae369634f8
Josua Müller <hypnotox@pm.me>
2026-06-10 23:46:42 +0200

    refactor: define Engine at file scope

--- mod/scripts/lib/engine.lua
+++ mod/scripts/lib/engine.lua
@@ -1,10 +1,8 @@
-function initEngine()
-    -- Holds game engine constants
-    Engine = {
-        voxelSize = 0.05
-    }
+-- Holds game engine constants
+Engine = {
+    voxelSize = 0.05
+}
 
-    function Engine:voxelCenterOffset()
-        return Transform(Vec(Engine.voxelSize * 0.5, Engine.voxelSize * 0.5, -Engine.voxelSize * 0.5))
-    end
+function Engine:voxelCenterOffset()
+    return Transform(Vec(Engine.voxelSize * 0.5, Engine.voxelSize * 0.5, -Engine.voxelSize * 0.5))
 end
